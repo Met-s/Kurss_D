22e5b8d7e1887853872b48fd019193f7f493c899
Met-s <den_vo@mail.ru>
2023-09-07 11:43:27 +0300

    Init

---
+++ django_d3/simpleapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+# Товар для витрины
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
