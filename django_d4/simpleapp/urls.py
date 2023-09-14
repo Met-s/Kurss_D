fd1a840d501dcd1eed4ed41c8ce2076acabad99b
Met-s <den_vo@mail.ru>
2023-09-14 17:00:38 +0300

    Фильтрация своя форма

--- django_d4/simpleapp/urls.py
+++ django_d4/simpleapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductsList, ProductDetail
+from .views import ProductsList, ProductDetail, ProductsForm
 
 
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns = [
     # pk - это первичный ключ товара, который будет выводиться у нас в шаблон
     # int - указывает на то, что принимаются только целочисленные значения
     path('<int:pk>', ProductDetail.as_view()),
+    path('products_form/', ProductsForm.as_view()),
 
 ]
--- django_d4/simpleapp/views.py
+++ django_d4/simpleapp/views.py
@@ -88,4 +88,19 @@ class ProductDetail(DetailView):
     context_object_name = 'product'
 
 
+class ProductsForm(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products_form.html'
+    context_object_name = 'products'
+    paginate_by = 2
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
