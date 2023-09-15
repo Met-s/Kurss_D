97573780f75bfda4f52b13c19cfec364421f6017
Met-s <den_vo@mail.ru>
2023-09-15 14:15:10 +0300

    Проверка данных одного поля формы

--- django_d4/simpleapp/forms.py
+++ django_d4/simpleapp/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'name',
+            'description',
+            'category',
+            'price',
+            'quantity',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        description = cleaned_data.get("description")
+        if description is not None and len(description) < 20:
+            raise ValidationError({
+                "description": "Описание не может быть меньше 20 символов."
+            })
+        return cleaned_data
