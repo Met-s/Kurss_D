1e12dc4517d4a7ef902842cbab402911b89339de
Met-s <den_vo@mail.ru>
2023-09-30 16:09:19 +0300

    D_6. Добавил, при регистрации пользователь получает письмо

--- django_d4/accounts/forms.py
+++ django_d4/accounts/forms.py
@@ -1,19 +1,37 @@
 from allauth.account.forms import SignupForm
-from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 
 
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
-        common_users = Group.objects.get(name="common users")
-        user.groups.add(common_users)
+
+        send_mail(
+            subject='Добро пожаловать в наш интернет-магазин!',
+            message=f'{user.username}, вы успешно зарегистрировались!',
+            from_email=None,  # Будет использованно значение DEFAULT_FROM_EMAIL
+            recipient_list=[user.email],
+        )
         return user
 
+# -----------------------------------------------------------------------------
+    # Эта форма при регистрации автоматически добавляет пользователя
+    # в группу "common users"
 
+# from allauth.account.forms import SignupForm
+# from django.contrib.auth.models import Group
+
+# class CustomSignupForm(SignupForm):
+    # def save(self, request):
+    #     user = super().save(request)
+    #     common_users = Group.objects.get(name="common users")
+    #     user.groups.add(common_users)
+    #     return user
+
+# -----------------------------------------------------------------------------
 # from django import forms
 # from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.models import User
-
 
 # class SignUpForm(UserCreationForm):
 #     email = forms.EmailField(label="Email")
