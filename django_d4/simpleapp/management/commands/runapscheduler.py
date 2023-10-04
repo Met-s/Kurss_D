9af1fef15387522adc5fe88facdb4a16063d3d26
Met-s <den_vo@mail.ru>
2023-10-04 16:39:19 +0300

    D_6. Рассылка по расписанию менеджерам самый дешёвый товар

---
+++ django_d4/simpleapp/management/commands/runapscheduler.py
@@ -0,0 +1,70 @@
+import logging
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+from django.core.mail import mail_managers
+from django.core.management.base import BaseCommand
+from django_apscheduler import util
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+from simpleapp.models import Product
+
+logger = logging.getLogger(__name__)
+
+
+def my_job():
+    print("DOROTY DAUN")
+    products = Product.objects.order_by('price')[:3]
+    text = '\n'.join(['{} - {}'.format(p.name, p.price) for p in products])
+    mail_managers("Самые дешёвые товары", text)
+
+
+@util.close_old_connections
+def delete_old_job_executions(max_age=604_800):
+    """
+    Это задание удаляет записи выполнения заданий APScheduler старше
+    максимального возраста 'max_age' из БД.
+    Это помогает предотвратить заполнение БД старыми историческими данными,
+    записи которые больше не нужны.
+    : param max_age: максимальная продолжительность хранения исторических
+    записей выполнения заданий. По умолчанию 7 дней.
+    """
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs APScheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            my_job,
+            trigger=CronTrigger(second="*/30"),
+            id="my_job",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'my_job'.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: 'delete_old_job_executions'.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
