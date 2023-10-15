243d9375fe0687045490ccd6b46e03e96e1b0591
Met-s <den_vo@mail.ru>
2023-10-15 19:20:39 +0300

    D_7. Redis работает

--- django_d4/django_d4/celery.py
+++ django_d4/django_d4/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'simpleapp')
+                      'django_d4.settings')
 
-app = Celery('simpleapp')
+app = Celery('django_d4')
 app.config_from_object('django.conf:settings',
                        namespace='CELERY')
 app.autodiscover_tasks()
